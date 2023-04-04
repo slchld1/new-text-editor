191ea93731e3e54da7ef067b967f51e945336878
Minjae Cho <113135811+slchld1@users.noreply.github.com>
2023-04-04 19:22:26 -0400

    database complete

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -14,33 +14,51 @@ const initdb = async () =>
 
 // adds logic to the 'put' method and adds it to database transaction as read and write.
 export const putDb = async (content) => {
-  console.log('PUT');
+  console.log('PUT initiated');
 
+  //create connection with jate
   const touchDb = await openDB('jate', 1);
-  const text = touchDb.transaction('jate', 'readwrite');
-  const store = text.objectStore('jate');
+
+  // new transaction of 'read and write' as our privilege
+  const tx = touchDb.transaction('jate', 'readwrite');
+
+  // Open desired obj store
+  const store = tx.objectStore('jate');
+
+  // put method to pass content
   const request = store.put({ id: 1, value: content});
 
+  // confirm request
   const result = await request;
 
+  // log for completion
   console.log('data saved.', result);
 }
 
 // adds logic for a method that gets all content in store from database to read only.
 export const getDb = async () => {
 
-  console.log('GET');
+  console.log('GET initiated');
 
+  // connect to database
   const touchDb = await openDB('jate', 1);
-  const text = touchDb.transaction('jate', 'readonly');
-  const store = text.objectStore('jate');
+  
+  // give a 'read only' privilege
+  const tx = touchDb.transaction('jate', 'readonly');
+
+  // open desired obj store
+  const store = tx.objectStore('jate');
+
+  // getAll() method to retrieve all saved databases
   const request = store.getAll();
 
+  // confirm the request
   const result = await request;
 
+  // log the result
   console.log('result.value', result)
-
-  return result?.value
+  // must return the value to update the editor every time it is open
+  return result.value
 };
 
 initdb();
